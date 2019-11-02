2b3d9c8bdce89bb4c540da9661939bb1aa3ef771
bwwiik <borge.wiik@hotmail.com>
2019-11-02 16:46:25 +0100

    First commit

---
+++ create-and-publish-blueprint.ps1
@@ -0,0 +1,45 @@
+param(
+    $managementgroupid = "AzureBlueprintMG",
+    $subscriptionId = "68561d79-60fb-4d83-9688-16314efefe17",
+    $blueprintName = "borgesBlueprint",
+    $servicePrincipalPass = "FOlm1A-c./0:1o3Cr7Vab]8AhozWQ8n]",
+    $applicationId = "db583b7d-abd6-4c0c-b929-f3754baf4b31",
+    $tenantId = "8b87af7d-8647-4dc7-8df4-5f69a2011bb5",
+    $SourceDir = "C:\src\Workshop_AzureBlueprints"
+)
+
+try{
+    # TODO Må laste inn az-moduler for de som ikke har de fra før
+
+    Write-Host "Start login with SPN"
+    $pass = ConvertTo-SecureString $servicePrincipalPass -AsPlainText -Force
+    $cred = New-Object -TypeName pscredential -ArgumentList $applicationId, $pass
+    Login-AzAccount -Credential $cred -ServicePrincipal -TenantId $tenantId
+    Write-Host "Successfully logged in with SPN"
+
+    Write-Host "Azure context:"
+    Get-AzContext
+    
+    Write-Host "Start Blueprint import"
+    Import-AzBlueprintWithArtifact -Name $blueprintName -SubscriptionId $subscriptionId -InputPath $SourceDir
+
+    if ($?) {
+        Write-Host "Imported successfully"
+    
+        $date = Get-Date -UFormat %Y%m%d.%H%M%S
+        $genVersion = "$date.TEST"
+        
+        $importedBp = Get-AzBlueprint -SubscriptionId $subscriptionId -Name $blueprintName 
+        Publish-AzBlueprint -Blueprint $importedBp -Version $genVersion
+    
+    } else {
+        throw "Failed to import successfully"
+        exit 1
+    }
+}
+catch
+{ 
+	if ($_.ErrorDetails.Message) {$ErrDetails = $_.ErrorDetails.Message } else {$ErrDetails = $_}
+ 	Write-Host "ERROR! $($ErrDetails)"
+	exit 1		
+}
